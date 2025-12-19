45b8bbf2698cdb451895af371cb3144c7df7362e
CoRnT04 <144427315+CoRnT04@users.noreply.github.com>
2025-12-19 07:52:55 +0100

    Update script_carte.js

--- script_carte.js
+++ script_carte.js
@@ -1,17 +1,26 @@
-let markers = [];
-
+// Créer la map
 const map = L.map('map').setView([20, 0], 2);
 
+// TileLayer
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; OpenStreetMap'
 }).addTo(map);
+
+// Créer un groupe de clusters
+const markersCluster = L.markerClusterGroup({
+    maxClusterRadius: 40,
+    showCoverageOnHover: false,
+    spiderfyOnMaxZoom: true
+});
+
+let allMarkers = [];
 
 // Charger les cocktails depuis JSON
 fetch('cocktails.json')
     .then(res => res.json())
     .then(cocktails => {
         cocktails.forEach(cocktail => {
-            const marker = L.marker([cocktail.lat, cocktail.lng]).addTo(map);
+            const marker = L.marker([cocktail.lat, cocktail.lng]);
             marker.alcool = cocktail.alcool;
 
             marker.bindPopup(`
@@ -20,23 +29,26 @@ fetch('cocktails.json')
                 <img src="${cocktail.image}" width="120">
             `);
 
-            // Animation simple au survol
             marker.on('mouseover', () => marker.setOpacity(0.6));
             marker.on('mouseout', () => marker.setOpacity(1));
 
-            markers.push(marker);
+            markersCluster.addLayer(marker);
+            allMarkers.push(marker);
         });
+
+        map.addLayer(markersCluster);
     });
 
 // Filtrage par alcool
 document.querySelectorAll('.filter-btn').forEach(btn => {
     btn.addEventListener('click', () => {
         const alcoolChoisi = btn.dataset.alcool;
-        markers.forEach(marker => {
+
+        markersCluster.clearLayers();
+
+        allMarkers.forEach(marker => {
             if (alcoolChoisi === "Tous" || marker.alcool === alcoolChoisi) {
-                if (!map.hasLayer(marker)) map.addLayer(marker);
-            } else {
-                if (map.hasLayer(marker)) map.removeLayer(marker);
+                markersCluster.addLayer(marker);
             }
         });
     });
